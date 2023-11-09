41664b0fcfcddf5fe68136723215386331a38290
MH Shifat <mhshifat757@gmail.com>
2023-11-09 13:59:33 +0600

    add open chrome with profiles and termius app

--- office_ini.sh
+++ office_ini.sh
@@ -2,15 +2,33 @@
 main_dir=$(pwd)
 
 #MongoDB Up
-./up_docker_mongo.sh
+#./up_docker_mongo.sh
 
 # open mongodb compass
-#mongodb-compass & # for linux
-/Applications/MongoDB\ Compass.app/Contents/MacOS/MongoDB\ Compass & # for mac
+mongodb-compass & # for linux
+
+#/Applications/MongoDB\ Compass.app/Contents/MacOS/MongoDB\ Compass & # for mac
+
 
 # open skype
-/Applications/Skype.app/Contents/MacOS/Skype &
+#/Applications/Skype.app/Contents/MacOS/Skype &
 
-cd "/Users/shifat/Documents/Projects/Office/inforeach-v2" || exit
+#postman
+postman &
+
+#chrome
+
+## Office profile
+google-chrome --profile-directory="Profile 1" &
+
+## Personal Profile
+google-chrome --profile-directory="Default" &
+
+#termius
+termius-app &
+
+#brave
+brave-browser &
+
+cd "/home/mh-shifat/Documents/DSi-Project/pdfmanager-poc/pdfmanager-poc" || exit
 idea . &
-
